5b91e671b57c46c3e4a61d29460a559df4b2cc17
vamsiiiiiiii <vamsi10kcoders@gmail.com>
2024-11-06 13:02:55 +0530

    storage and simple website with login feature

---
+++ js.js
@@ -0,0 +1,19 @@
+// localStorage.setItem("name","vamsi");
+// localStorage.setItem("password","vamsi@123")
+
+// let inputN=document.getElementById("name")
+// let inputP=document.getElementById("password")
+// let n=localStorage.getItem("name");
+// let p=localStorage.getItem("password");
+
+// function login (event){
+//     event.preventDefault()
+//     if(inputN.value === n && inputP.value === p){
+//         window.location.href="home.html"
+//         alert("succesfully login")
+        
+//     }else{
+//         alert("not login !!!    ")
+//     }
+// }
+// document.body.style.transform
